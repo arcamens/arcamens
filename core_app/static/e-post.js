92fa0a4b50acb04abe1ef4d9216715d0f9c6d994
Iury de Oliveira Gomes Figueiredo <ioliveira@id.uff.br>
2018-02-08 20:57:41 +0000

    Fixing bug with card/post creation, avoiding click event being fired multiple times.

--- core_app/static/e-post.js
+++ core_app/static/e-post.js
@@ -1,4 +1,4 @@
-function do_post() {
+function do_post(e) {
     shell = $(this).attr('data-shell');
     shell_error = $(this).attr('data-shell-error');
 
@@ -13,20 +13,27 @@ function do_post() {
     $.ajax({
     url: url,  //Server script to process data
     type: 'POST',
+
     success: function(data) {
         eval(callback);
         $(shell).html(data);
+        $(document).on('click', '.e-post', do_post);
+
     },
+
     error: function(data){
         eval(callback_error);
         $(shell_error).html(data.responseText);
-
+        $(document).on('click', '.e-post', do_post);
     },
     data: formData,
     cache: false,
     contentType: false,
-    processData: false
+    processData: false,
     });
+
+    $(document).off('click', '.e-post', do_post);
 }
 
 $(document).on('click', '.e-post', do_post);
+
