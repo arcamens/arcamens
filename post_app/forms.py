73af0dc16e16eb8646b76f8ae23b279c6fda8639
Iury de Oliveira Gomes Figueiredo <ioliveira@id.uff.br>
2018-03-10 03:55:52 +0000

    Fixing displaying of post.

--- post_app/forms.py
+++ post_app/forms.py
@@ -4,15 +4,13 @@ from . import models
 class PostForm(forms.ModelForm):
     class Meta:
         model  = models.Post
-        exclude = ('user', 'ancestor', 'html')
+        exclude = ('user', 'ancestor', 
+            'html', 'parent')
 
 class PostFileWrapperForm(forms.ModelForm):
     class Meta:
         model  = models.PostFileWrapper
         exclude = ('post', )
-
-class AssignPostForm(forms.Form):
-    email = forms.EmailField()
 
 class PostFilterForm(forms.ModelForm):
     class Meta:
@@ -26,15 +24,17 @@ class GlobalPostFilterForm(forms.ModelForm):
 
 class UserSearchForm(forms.Form):
     pattern = forms.CharField(required=False,
-    help_text='Example: victor + #arcamens \
-    + #suggestion ...')
+    help_text='Example: victor + #arcamens + #suggestion ...')
 
 class TagSearchForm(forms.Form):
     name = forms.CharField(required=False)
 
+class AssignPostForm(forms.Form):
+    email = forms.EmailField()
 
 class PostAttentionForm(forms.Form):
     message = forms.CharField(
     required=False, widget=forms.Textarea,
     help_text='(Optional) Be in the meeting 20 min earlier.')
+
 
