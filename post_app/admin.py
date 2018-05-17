166bc5fb6287a82dbfe40f78e845797ffb67ca5a
Iury de Oliveira Gomes Figueiredo <ioliveira@id.uff.br>
2018-05-17 17:08:59 +0000

    Adding post app to admin site.

--- post_app/admin.py
+++ post_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(models.Post)
+admin.site.register(models.GlobalPostFilter)
+admin.site.register(models.GlobalAssignmentFilter)
+
