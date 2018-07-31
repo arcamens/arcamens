99a1ffe926be3b7694d60e7ae40a31a3415f720c
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2018-07-31 15:00:32 -0300

    https://staging.arcamens.com/card_app/card-link/56/

--- core_app/models.py
+++ core_app/models.py
@@ -229,17 +229,18 @@ class Node(models.Model):
     indexer = models.AutoField(primary_key=True)
 
 class Organization(OrganizationMixin):
-    name     = models.CharField(null=True,
-    blank=False, verbose_name=_("Name"),  max_length=256)
+    name = models.CharField(null=False,
+    blank=False, verbose_name=_("Name"),  max_length=100)
+
     expiration = models.DateTimeField(blank=True, null=True)
 
     owner = models.ForeignKey('User', null=True, 
     related_name='owned_organizations', blank=True)
+
     created = models.DateTimeField(auto_now=True, null=True)
 
-    admins = models.ManyToManyField(
-    'User', related_name='managed_organizations', 
-    null=True, blank=True, symmetrical=False)
+    admins = models.ManyToManyField('User', symmetrical=False,
+    related_name='managed_organizations')
 
 class Period(BasicItem):
     """
@@ -248,7 +249,7 @@ class Period(BasicItem):
 
     # The price should be calculated taking into account
     # User.expiration and current User.max_users attrs.
-    price = models.IntegerField(null=True, default=0)
+    price = models.IntegerField(null=False, default=0)
 
     # This is the max number of users that our customer
     # will purchase for a period of time. There is a difference
@@ -256,14 +257,14 @@ class Period(BasicItem):
     # attempt to add more users to his account than the max then he is
     # asked to upgrade his limits. This way i think we may be able
     # to implement subscription.
-    max_users = models.IntegerField(null=True, default=3,
+    max_users = models.IntegerField(null=False, default=3,
     help_text="Max users until the expiration.")
 
     expiration = models.DateField(null=True, 
     blank=False, help_text="Example: year-month-day")
 
-    total = models.FloatField(null=True, default=0)
-    paid  = models.BooleanField(blank=True, default=False)
+    total = models.FloatField(null=False, default=0)
+    paid  = models.BooleanField(default=False)
 
     def __str__(self):
         return ('Paid: {paid}' 
@@ -276,34 +277,30 @@ class Period(BasicItem):
 class Invite(InviteMixin):
     # email = models.EmailField(max_length=70, 
     # null=True, blank=False)
-    user = models.ForeignKey('core_app.User', null=True, 
-    blank=True, related_name='invites')
+    user = models.ForeignKey('core_app.User', 
+    null=False, related_name='invites')
 
-    peer = models.ForeignKey('core_app.User', null=True, 
-    blank=True, related_name='sent_invites')
+    peer = models.ForeignKey('core_app.User', null=False, 
+    related_name='sent_invites')
 
     # should have a count to avoid mail spam.
-    token = models.CharField(null=True,
-    blank=False, max_length=256)
-
-    invite_url = models.CharField(null=True,
-    blank=False, max_length=256)
+    token = models.CharField(null=False, blank=False, max_length=256)
+    invite_url = models.CharField(null=False, blank=False, max_length=256)
 
     organization = models.ForeignKey('Organization', 
-    null=True, blank=True, related_name='invites')
-    created = models.DateTimeField(auto_now=True, null=True)
+    null=False, related_name='invites')
+
+    created = models.DateTimeField(auto_now=True, null=False)
 
 class User(UserMixin, BasicUser):
     organizations = models.ManyToManyField(
-    'Organization', related_name='users', 
-    null=True, blank=True, symmetrical=False)
+    'Organization', related_name='users', symmetrical=False)
 
-    tags = models.ManyToManyField(
-    'Tag', related_name='users', 
-    null=True, blank=True, symmetrical=False)
+    tags = models.ManyToManyField('Tag', 
+    related_name='users', symmetrical=False)
 
-    default = models.ForeignKey('Organization', 
-    null=True, blank=True, on_delete=models.SET_NULL)
+    default = models.ForeignKey('Organization',
+    null=True, on_delete=models.SET_NULL)
 
     description = models.CharField(null=True,
     blank=False, verbose_name=_("Bio"), 
@@ -313,31 +310,30 @@ class User(UserMixin, BasicUser):
     avatar = models.ImageField(null=True,
     verbose_name='Your avatar.', help_text='', blank=True)
 
-    enabled = models.BooleanField(blank=True, default=False)
+    enabled = models.BooleanField(default=False)
 
-    c_storage   = models.IntegerField(null=True, default=0)
-    c_download = models.IntegerField(null=True, default=0)
+    c_storage   = models.IntegerField(null=False, default=0)
+    c_download = models.IntegerField(null=False, default=0)
 
     # default for expiration...
     # default=datetime.date.today() + datetime.timedelta(0)
-    max_users  = models.IntegerField(null=True, default=3)
-    paid       = models.BooleanField(blank=True, default=False)
+    max_users  = models.IntegerField(null=False, default=3)
+    paid       = models.BooleanField(null=False, default=False)
     expiration = models.DateField(null=True)
 
 class Event(EventMixin):
-    users = models.ManyToManyField('core_app.User', null=True,  
-    related_name='events', blank=True, symmetrical=False)
+    users = models.ManyToManyField('core_app.User',   
+    related_name='events', symmetrical=False)
 
     organization = models.ForeignKey('Organization', 
-    related_name='events', null=True, blank=True)
+    related_name='events', null=True)
 
     # created = models.DateTimeField(auto_now=True, null=True)
-    created = models.DateTimeField(auto_now=True, null=True)
+    created = models.DateTimeField(auto_now=True, null=False)
+    user    = models.ForeignKey('core_app.User', null=True)
 
-    user = models.ForeignKey('core_app.User', null=True, blank=True)
-
-    signers = models.ManyToManyField('core_app.User', null=True,  
-    related_name='seen_events', blank=True, symmetrical=False)
+    signers = models.ManyToManyField('core_app.User', 
+    related_name='seen_events', symmetrical=False)
 
     html = models.TextField(null=True, blank=False)
     html_template = None
@@ -346,58 +342,57 @@ class Event(EventMixin):
         return 'Event'
 
 class Tag(TagMixin):
-    name = models.CharField(null=True,
-    blank=False, max_length=70)
+    name = models.CharField(null=False, blank=False, max_length=100)
 
-    description = models.CharField(null=True,
-    blank=False, default='...', max_length=256)
+    description = models.CharField(null=True, blank=False, 
+    default='...', max_length=256)
 
     # When the organization is deleted all its tags
     # are deleted too.
     organization = models.ForeignKey('core_app.Organization',
-    related_name='tags', null=True, blank=True)
+    related_name='tags', null=False)
 
     class Meta:
         unique_together = ('name', 'organization')
 
 class EInviteUser(Event):
-    peer = models.ForeignKey('User', null=True, 
-    related_name='e_invite_user0', blank=True)
+    peer = models.ForeignKey('User', null=False, 
+    related_name='e_invite_user0')
     html_template = 'core_app/e-invite-user.html'
 
 class EShout(EShoutMixin, Event):
-    msg = models.CharField(null=True,
+    msg = models.CharField(null=False,
     blank=False, verbose_name=_("Msg"),  max_length=256,
     help_text="No pain no gain!")
     html_template = 'core_app/e-shout.html'
 
 class EJoinOrganization(Event):
-    peer = models.ForeignKey('User', null=True, 
-    related_name='e_join_organization0', blank=True)
+    peer = models.ForeignKey('User', null=False, 
+    related_name='e_join_organization0')
     html_template = 'core_app/e-join-organization.html'
 
 class EBindUserTag(Event):
-    peer = models.ForeignKey('User', null=True, 
-    related_name='e_bind_user_tag0', blank=True)
-    tag = models.ForeignKey('Tag', null=True, blank=True)
+    peer = models.ForeignKey('User', null=False, 
+    related_name='e_bind_user_tag0')
+
+    tag = models.ForeignKey('Tag', null=False)
     html_template = 'core_app/e-bind-user-tag.html'
 
 class ECreateTag(Event):
-    tag = models.ForeignKey('Tag', null=True, 
-    related_name='e_create_tag1', blank=True)
+    tag = models.ForeignKey('Tag', null=False, 
+    related_name='e_create_tag1')
     html_template = 'core_app/e-create-tag.html'
 
 class EDeleteTag(Event):
-    tag_name = models.CharField(null=True,
-    blank=False, max_length=256)
+    tag_name = models.CharField(null=False, max_length=256)
     html_template = 'core_app/e-delete-tag.html'
 
 class EUnbindUserTag(Event):
-    peer = models.ForeignKey('User', null=True, 
-    related_name='e_unbind_user_tag0', blank=True)
-    tag = models.ForeignKey('Tag', null=True, blank=True)
-    html_template = 'core_app/e-unbind-user-tag.html'
+    peer = models.ForeignKey('User', null=False, 
+    related_name='e_unbind_user_tag0')
 
+    tag = models.ForeignKey('Tag', null=False)
+    html_template = 'core_app/e-unbind-user-tag.html'
 
 class UserFilter(models.Model):
     organization = models.ForeignKey('core_app.Organization', 
@@ -406,8 +401,7 @@ class UserFilter(models.Model):
     pattern  = models.CharField(max_length=255, blank=True, default='',
     help_text='Example: oliveira@arcamens.com')
 
-    user = models.ForeignKey('core_app.User', 
-    null=True, blank=True)
+    user = models.ForeignKey('core_app.User', null=False)
 
     # It warrants there will exist only one user and organization
     # filter. If we decide to permit more filters..
@@ -415,22 +409,17 @@ class UserFilter(models.Model):
         unique_together = ('user', 'organization',)
 
 class Clipboard(models.Model):
-    organization = models.ForeignKey(
-    'core_app.Organization', blank=True)
+    organization = models.ForeignKey('core_app.Organization', null=False)
 
-    user = models.ForeignKey('core_app.User', blank=True)
-
-    posts = models.ManyToManyField(
-    'post_app.Post', null=True, blank=True, 
+    user  = models.ForeignKey('core_app.User', null=False)
+    posts = models.ManyToManyField('post_app.Post', 
     related_name='post_clipboard_users', symmetrical=False)
 
-    cards= models.ManyToManyField(
-    'card_app.Card', related_name='card_clipboard_users', 
-    null=True, blank=True, symmetrical=False)
+    cards = models.ManyToManyField('card_app.Card', 
+    related_name='card_clipboard_users', symmetrical=False)
 
-    lists = models.ManyToManyField(
-    'list_app.List', related_name='list_clipboard_users', 
-    null=True, blank=True, symmetrical=False)
+    lists = models.ManyToManyField('list_app.List', 
+    related_name='list_clipboard_users', symmetrical=False)
 
     class Meta:
         unique_together = ('user', 'organization')
@@ -439,10 +428,10 @@ class EUpdateOrganization(Event):
     html_template = 'core_app/e-update-organization.html'
 
 class ERemoveOrganizationUser(Event):
-    peer = models.ForeignKey('User', null=True, 
-    related_name='e_remove_organization_user0', blank=True)
+    peer = models.ForeignKey('User', null=False, 
+    related_name='e_remove_organization_user0')
 
-    reason = models.CharField(null=True, default='',
+    reason = models.CharField(null=False, default='',
     blank=True, max_length=256)
 
     html_template = 'core_app/e-remove-organization-user.html'
@@ -451,10 +440,9 @@ class NodeFilter(models.Model):
     pattern = models.CharField(max_length=255, blank=True, 
     default='', help_text='/projectname/', )
 
-    user = models.ForeignKey('core_app.User', null=True, blank=True)
-    organization = models.ForeignKey('core_app.Organization', 
-    blank=True, null=True)
+    user = models.ForeignKey('core_app.User', null=False)
 
+    organization = models.ForeignKey('core_app.Organization', null=False)
     status = models.BooleanField(blank=True, default=False, 
     help_text='Filter On/Off.')
 
@@ -467,9 +455,9 @@ class EventFilter(models.Model):
     start = models.DateField(null=True, default=datetime.now, blank=False)
     end   = models.DateField(null=True, default=datetime.now, blank=False)
 
-    user = models.ForeignKey('core_app.User', null=True, blank=True)
-    organization = models.ForeignKey('core_app.Organization', 
-    blank=True, null=True)
+    user = models.ForeignKey('core_app.User', null=False)
+
+    organization = models.ForeignKey('core_app.Organization', null=False)
 
     # It warrants there will exist only one user and organization
     # filter. If we decide to permit more filters..
