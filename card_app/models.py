f04d7cd7876b4b4b2d828da4d22840631c6010b5
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2018-08-02 14:40:54 -0300

    https://staging.arcamens.com/card_app/card-link/775/

--- card_app/models.py
+++ card_app/models.py
@@ -755,6 +755,18 @@ class ESetCardDeadline(Event):
 
     html_template = 'card_app/e-set-card-deadline.html'
 
+class EArrivedCardDeadline(Event):
+    ancestor = models.ForeignKey('list_app.List', 
+    related_name='e_arrived_card_deadline0', blank=True)
+
+    card = models.ForeignKey('card', 
+    related_name='e_arrived_card_deadline0', blank=True)
+
+    board = models.ForeignKey('board_app.Board', 
+    related_name='e_arrived_card_deadline0', blank=True)
+
+    html_template = 'card_app/e-arrived-card-deadline.html'
+
 @receiver(pre_delete, sender=CardFileWrapper)
 def delete_filewrapper(sender, instance, **kwargs):
     is_unique = CardFileWrapper.objects.filter(file=instance.file)
@@ -767,6 +779,7 @@ def clean_disk(record):
     org.owner.c_storage = F('c_storage') + record.file.size
     org.owner.save()
     record.file.delete(save=False)
+
 
 
 
---
+++ core_app/management/commands/check_deadlines.py
@@ -0,0 +1,37 @@
+from django.core.management import BaseCommand
+from core_app.models import User
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from card_app.models import Card, EArrivedCardDeadline
+from django.db.models import Q, F
+from django.utils import timezone
+from django.conf import settings
+
+arcabot, _ = User.objects.get_or_create(
+email=settings.ARCAMENS_BOT_EMAIL, name=settings.ARCAMENS_BOT_NAME)
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        now   = timezone.now()
+        query = Q(deadline__lte=now)
+        cards = Card.objects.filter(query)
+        cards  = cards.only('id', 'label', 'workers')
+    
+        for ind in cards:
+            self.run_timeout(ind)
+        self.stdout.write('Checked deadlines!')
+    
+    def run_timeout(self, card):
+        url    = reverse('card_app:card-link', kwargs={'card_id': card.id})
+        emails = card.workers.values_list('email')
+    
+        send_mail('Task deadline has arrived', '%s %s' % (card.label, url),
+                'noreply@arcamens.com', emails, fail_silently=False)
+    
+        event = EArrivedCardDeadline.objects.create(
+        organization=card.ancestor.ancestor.organization, card=card, 
+        ancestor=card.ancestor, board=card.ancestor.ancestor, user=arcabot)
+    
+        event.dispatch(*card.workers.all(), *card.ancestor.ancestor.members.all())
+    
+
