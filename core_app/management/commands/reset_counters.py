4bf03ab86a8c18da2d4445c171282d40a2eacfa4
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2018-08-12 14:48:45 -0300

    Fixing bug with counters.

--- core_app/management/commands/reset_counters.py
+++ core_app/management/commands/reset_counters.py
@@ -5,11 +5,12 @@ from django.conf import settings
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         users = User.objects.filter(enabled=True)
-        users.update(c_storage=0, c_download=0)
+        users.update(c_download=0)
         self.stdout.write('Counters were reseted!')
     
 
     
+
 
 
 
