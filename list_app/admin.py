78e6a2cde7fc5cd2e6c422ab9624a2674b5ef427
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2018-08-13 12:58:41 -0300

    https://staging.arcamens.com/card_app/card-link/797/

--- list_app/admin.py
+++ list_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.ListPin)
+admin.site.register(models.List)
+admin.site.register(models.ECreateList)
+admin.site.register(models.EUpdateList)
+admin.site.register(models.EDeleteList)
+admin.site.register(models.ECutList)
+admin.site.register(models.ECopyList)
+admin.site.register(models.EPasteCard)
+
+
