3c942b4549f0b124cceaba0726a26fbb54260693
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2018-08-13 13:11:13 -0300

    https://staging.arcamens.com/card_app/card-link/797/

--- group_app/admin.py
+++ group_app/admin.py
@@ -5,14 +5,10 @@ from . import models
 admin.site.register(models.Group)
 admin.site.register(models.ECreateGroup)
 admin.site.register(models.EDeleteGroup)
-
-
-
-
-
-
-
-
-
-
+admin.site.register(models.GroupPin)
+admin.site.register(models.Groupship)
+admin.site.register(models.EUpdateGroup)
+admin.site.register(models.EBindGroupUser)
+admin.site.register(models.EUnbindGroupUser)
+admin.site.register(models.EPastePost)
 
